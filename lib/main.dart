6832a20d4c3691901e30d5718ec7df100bfb4a06
Yann Le Vagueres <yann.lv@live.fr>
2026-06-20 18:02:26 +0200

    Starting mockup

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
+import 'package:fluent_ui/fluent_ui.dart';
+import 'package:flutter/foundation.dart';
 import 'package:rinf/rinf.dart';
+import 'package:window_manager/window_manager.dart';
+import 'dart:ui' as ui;
+
 import 'src/bindings/bindings.dart';
-import 'package:flutter/material.dart';
 
 Future<void> main() async {
   await initializeRust(assignRustSignal);
@@ -13,25 +17,11 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return FluentApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      theme: FluentThemeData(
+        brightness: Brightness.dark,
+        accentColor: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
@@ -78,69 +68,48 @@ class _MyHomePageState extends State<MyHomePage> {
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-            ElevatedButton(
-              onPressed: () async {
-                MyPreciousData(
-                  inputNumbers: [3, 4, 5],
-                  inputString: 'Zero-cost abstraction',
-                ).sendSignalToRust(); // GENERATED
-              },
-              child: Text('Send a Signal from Dart to Rust'),
-            ),
-            StreamBuilder(
-              stream: MyAmazingNumber.rustSignalStream,
-              builder: (context, snapshot) {
-                final signalPack = snapshot.data;
-                if (signalPack == null) {
-                  return Text('Nothing received yet');
-                }
-                final myAmazingNumber = signalPack.message;
-                final currentNumber = myAmazingNumber.currentNumber;
-                return Text(currentNumber.toString());
-              },
-            ),
-          ],
+    return Container(
+      height: MediaQuery.of(context).size.height * 0.7,
+      width: MediaQuery.of(context).size.width * 0.9,
+      // Below is the code for Linear Gradient.
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [ui.Color(0xFFff0058), ui.Color(0xFFffbc00)],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      child: Column(
+        children: [
+          Column(children: [Text("Activity\ntracker")]),
+          Column(children: [WindowButtons()]),
+        ],
+      ),
     );
+  }
+}
+
+class WindowButtons extends StatelessWidget {
+  const WindowButtons({super.key});
+
+  @override
+  Widget build(final BuildContext context) {
+    final theme = FluentTheme.of(context);
+
+    switch (defaultTargetPlatform) {
+      case TargetPlatform.windows:
+      case TargetPlatform.macOS:
+      case TargetPlatform.linux:
+        return SizedBox(
+          width: 138,
+          height: 50,
+          child: WindowCaption(
+            brightness: theme.brightness,
+            backgroundColor: Colors.transparent,
+          ),
+        );
+      default:
+        return const SizedBox.shrink();
+    }
   }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,10 @@
 import FlutterMacOS
 import Foundation
 
+import screen_retriever_macos
+import window_manager
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  ScreenRetrieverMacosPlugin.register(with: registry.registrar(forPlugin: "ScreenRetrieverMacosPlugin"))
+  WindowManagerPlugin.register(with: registry.registrar(forPlugin: "WindowManagerPlugin"))
 }
