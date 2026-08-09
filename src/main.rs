1ae707811b14340c45e31c95fb508d5dc54c40d0
Yann Le Vagueres <yann.lv@live.fr>
2026-08-09 13:32:05 +0200

    styled rules app

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,32 @@
-use iced::{
-    widget::{slider, column, progress_bar, text, ProgressBar},
-};
+use iced::{Color, border::Radius, widget::{Rule, column, rule::{self, FillMode, horizontal as horizontal_rule, vertical as vertical_rule}, text}};
 
 fn main() -> iced::Result {
-    iced::run(MyApp::update, MyApp::view)
+    iced::run( MyApp::update, MyApp::view)
 }
 
 #[derive(Debug, Clone)]
-enum Message {
-    UpdateValue(u32),
-}
+enum Message {}
 
-struct MyApp {
-    value: u32,
-}
-
-impl Default for MyApp {
-    fn default() -> Self {
-        Self {
-            value: 50
-        }
-    }
-}
+#[derive(Default)]
+struct MyApp;
 
 impl MyApp {
-    fn update(&mut self, message: Message) {
-        match message {
-            Message::UpdateValue(v) => self.value = v,
-        }
-    }
+    fn update(&mut self, _message: Message) {}
 
-    fn view(&self) -> iced::Element<'_, Message> {
+    fn view(&self) -> iced::Element<Message> {
         column![
             text("Construct from struct"),
-            ProgressBar::new(0.0..=100.0, self.value as f32 + 20.),
             text("Construct from function"),
-            progress_bar(0.0..=100.0, self.value as f32),
-            text(format!("Functional slider {}", self.value)),
-            slider(0..=100, self.value, Message::UpdateValue),
+            horizontal_rule(0),
+            text("Different space"),
+            horizontal_rule(5).style(|_| rule::Style {
+                radius: Radius::new(5),
+                snap: true,
+                fill_mode: FillMode::AsymmetricPadding(50, 20),
+                color: Color::from_rgb8(255, 0, 0)
+            }),
+            text("Vertical rule"),
+            vertical_rule(100),
         ]
         .into()
     }
