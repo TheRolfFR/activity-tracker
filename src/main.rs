3f10663ea3a40b6b21f3df050f38378ad239062d
Yann Le Vagueres <yann.lv@live.fr>
2026-08-09 16:54:09 +0200

    multi window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use std::collections::BTreeMap;
+
+use iced::widget::{
+    button, center, center_x, column, container, operation, scrollable, space, text, text_input,
+};
+use iced::window;
+use iced::{Center, Fill, Function, Subscription, Task, Vector};
+
 use iced_fluent_theme::{
     BrandVariants, Theme,
     font::{self}
@@ -15,39 +23,179 @@ fn main() -> iced::Result {
         ..Default::default()
     };
 
-    iced::application(MyApp::new, MyApp::update, MyApp::view)
+    iced::daemon(MyApp::new, MyApp::update, MyApp::view)
+        .subscription(MyApp::subscription)
         .settings(settings)
         .title(MyApp::title)
         .theme(MyApp::theme)
+        .scale_factor(MyApp::scale_factor)
         .run()
 }
 
 struct MyApp {
+    windows: BTreeMap<window::Id, Window>,
 }
 
-#[derive(Clone, Debug)]
+#[derive(Debug)]
+struct Window {
+    title: String,
+    scale_input: String,
+    current_scale: f32,
+}
+
+#[derive(Debug, Clone)]
 enum Message {
+    OpenWindow,
+    WindowOpened(window::Id),
+    WindowClosed(window::Id),
+    ScaleInputChanged(window::Id, String),
+    ScaleChanged(window::Id, String),
+    TitleChanged(window::Id, String),
 }
 
 impl MyApp {
-    fn new() -> Self {
+    fn new() -> (Self, Task<Message>) {
+        let (_, open) = window::open(window::Settings::default());
 
-        Self {
+        (
+            Self {
+                windows: BTreeMap::new(),
+            },
+            open.map(Message::WindowOpened),
+        )
+    }
+
+    fn update(&mut self, message: Message) -> Task<Message> {
+        match message {
+            Message::OpenWindow => {
+                let Some(last_window) = self.windows.keys().last() else {
+                    return Task::none();
+                };
+
+                window::position(*last_window)
+                    .then(|last_position| {
+                        let position =
+                            last_position.map_or(window::Position::Default, |last_position| {
+                                window::Position::Specific(last_position + Vector::new(20.0, 20.0))
+                            });
+
+                        let (_, open) = window::open(window::Settings {
+                            position,
+                            ..window::Settings::default()
+                        });
+
+                        open
+                    })
+                    .map(Message::WindowOpened)
+            }
+            Message::WindowOpened(id) => {
+                let window = Window::new(self.windows.len() + 1);
+                let focus_input = operation::focus(format!("input-{id}"));
+
+                self.windows.insert(id, window);
+
+                focus_input
+            }
+            Message::WindowClosed(id) => {
+                self.windows.remove(&id);
+
+                if self.windows.is_empty() {
+                    iced::exit()
+                } else {
+                    Task::none()
+                }
+            }
+            Message::ScaleInputChanged(id, scale) => {
+                if let Some(window) = self.windows.get_mut(&id) {
+                    window.scale_input = scale;
+                }
+
+                Task::none()
+            }
+            Message::ScaleChanged(id, scale) => {
+                if let Some(window) = self.windows.get_mut(&id) {
+                    window.current_scale = scale
+                        .parse()
+                        .unwrap_or(window.current_scale)
+                        .clamp(0.5, 5.0);
+                }
+
+                Task::none()
+            }
+            Message::TitleChanged(id, title) => {
+                if let Some(window) = self.windows.get_mut(&id) {
+                    window.title = title;
+                }
+
+                Task::none()
+            }
         }
     }
 
-    fn update(&mut self, _message: Message) {
+    fn view(&self, window_id: window::Id) -> Element<'_, Message> {
+        if let Some(window) = self.windows.get(&window_id) {
+            center(window.view(window_id)).into()
+        } else {
+            space().into()
+        }
     }
 
-    fn view(&self) -> Element<'_, Message> {
-        "content".into()
+
+    fn title(&self, window: window::Id) -> String {
+        self.windows
+            .get(&window)
+            .map(|window| window.title.clone())
+            .unwrap_or_default()
     }
 
-    fn title(&self) -> String {
-        String::from("Iced Fluent Theme Gallery")
-    }
-
-    fn theme(&self) -> Theme {
+    fn theme(&self, _window: window::Id) -> Theme {
         Theme::light(Some(BrandVariants::DEFAULT))
+    }
+
+    fn scale_factor(&self, window: window::Id) -> f32 {
+        self.windows
+            .get(&window)
+            .map(|window| window.current_scale)
+            .unwrap_or(1.0)
+    }
+
+    fn subscription(&self) -> Subscription<Message> {
+        window::close_events().map(Message::WindowClosed)
+    }
+}
+
+impl Window {
+    fn new(count: usize) -> Self {
+        Self {
+            title: format!("Window_{count}"),
+            scale_input: "1.0".to_string(),
+            current_scale: 1.0,
+        }
+    }
+
+    fn view(&self, id: window::Id) -> Element<'_, Message> {
+        let scale_input = column![
+            text("Window scale factor:"),
+            text_input("Window Scale", &self.scale_input)
+                .on_input(Message::ScaleInputChanged.with(id))
+                .on_submit(Message::ScaleChanged(id, self.scale_input.to_string()))
+        ];
+
+        let title_input = column![
+            text("Window title:"),
+            text_input("Window Title", &self.title)
+                .on_input(Message::TitleChanged.with(id))
+                .id(format!("input-{id}"))
+        ];
+
+        let new_window_button = button(text("New Window")).on_press(Message::OpenWindow);
+
+        let content = column![scale_input, title_input, new_window_button]
+            .spacing(50)
+            .width(Fill)
+            .align_x(Center)
+            .width(200);
+
+        container(scrollable(center_x(content))).padding(10).into()
     }
 }
