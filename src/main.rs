8e87d166077664c4b6648d3293f3cdb12027784c
Yann Le Vagueres <yann.lv@live.fr>
2026-08-09 12:42:26 +0200

    checkbox app

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,93 @@
-use iced::widget::{button, row, text, Row};
+use iced::{
+    Font,
+    font::Family,
+    widget::{
+        Checkbox, checkbox,
+        checkbox::Icon,
+        column,
+        text::{LineHeight, Shaping},
+    },
+};
 
-struct Counter {
-    value: u8,
+fn main() -> iced::Result {
+    iced::run( MyApp::update, MyApp::view)
 }
 
-impl Default for Counter {
-  fn default() -> Self {
-      Self { value: 255 }
-  }
-}
-
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone)]
 enum Message {
-    Increment,
-    Decrement,
+    DoNothing,
+    Update4(bool),
+    Update5(bool),
 }
 
-impl Counter {
+#[derive(Default)]
+struct MyApp {
+    checkbox4: bool,
+    checkbox5: bool,
+}
+
+impl MyApp {
     fn update(&mut self, message: Message) {
         match message {
-            Message::Increment => {
-                self.value = self.value.checked_add(1).unwrap_or(self.value);
-            }
-            Message::Decrement => {
-                self.value = self.value.checked_sub(1).unwrap_or(self.value);
-            }
+            Message::DoNothing => {}
+            Message::Update4(b) => self.checkbox4 = b,
+            Message::Update5(b) => self.checkbox5 = b,
         }
     }
-}
 
-#[test]
-fn it_counts_properly() {
-    let mut counter = Counter { value: 0 };
-
-    counter.update(Message::Increment);
-    counter.update(Message::Increment);
-    counter.update(Message::Decrement);
-
-    assert_eq!(counter.value, 1);
-}
-
-impl Counter {
-    fn view(&self) -> Row<Message> {
-        row![
-            button("+").on_press(Message::Increment),
-            text(self.value),
-            button("-").on_press(Message::Decrement),
+    fn view(&self) -> iced::Element<Message> {
+        column![
+            Checkbox::new(false)
+              .label("Construct from struct"),
+            checkbox(false)
+              .label("Construct from function"),
+            checkbox(false)
+                .label("Enabled checkbox")
+              .on_toggle(|_| Message::DoNothing),
+            checkbox(self.checkbox4)
+                .label("Functional checkbox")
+              .on_toggle(|b| Message::Update4(b)),
+            checkbox(self.checkbox5)
+                .label("Shorter parameter")
+              .on_toggle(Message::Update5),
+            checkbox(false)
+                .label("Larger box")
+                .on_toggle(|_| Message::DoNothing)
+                .size(30),
+            checkbox(true)
+                .label("Different icon")
+                .on_toggle(|_| Message::DoNothing)
+                .icon(Icon {
+                    font: Font::DEFAULT,
+                    code_point: '*',
+                    size: None,
+                    line_height: LineHeight::default(),
+                    shaping: Shaping::default()
+                }),
+            checkbox(false)
+                .label("Different font")
+                .on_toggle(|_| Message::DoNothing)
+                .font(Font {
+                    family: Family::Fantasy,
+                    ..Font::DEFAULT
+                }),
+            checkbox(false)
+                .label("Larger text")
+                .on_toggle(|_| Message::DoNothing)
+                .text_size(24),
+            checkbox(false)
+                .label("Special character 😊")
+                .on_toggle(|_| Message::DoNothing)
+                .text_shaping(Shaping::Advanced),
+            checkbox(false)
+                .label("Space between box and text")
+                .on_toggle(|_| Message::DoNothing)
+                .spacing(30),
+            checkbox(false)
+                .label("Space between box and text")
+                .on_toggle(|_| Message::DoNothing)
+                .spacing(0),
         ]
+        .into()
     }
-}
-
-pub fn main() -> iced::Result {
-    iced::run(Counter::update, Counter::view)
 }
