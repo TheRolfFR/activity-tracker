2611ef300d16391de5754b680b39f6fd5d589921
Yann Le Vagueres <yann.lv@live.fr>
2026-08-13 21:12:44 +0200

    window controls looking good

--- src/window_controls/mod.rs
+++ src/window_controls/mod.rs
@@ -1,16 +1,22 @@
-
-use iced::{Color, widget::{button, row, svg::{Handle, Svg}}, window};
+use iced::{
+    alignment::{Horizontal, Vertical},
+    widget::{
+        button, row,
+        svg::{self, Handle, Svg},
+        Container,
+    },
+    window, Color, Length,
+};
 use iced_fluent_theme::button::medium;
 use std::sync::LazyLock;
 
-use crate::Element;
 use crate::state::MainMessage;
+use crate::Element;
 
 macro_rules! icon {
     ($name:ident, $svg:literal) => {
-        static $name: LazyLock<Handle> = LazyLock::new(|| {
-            Handle::from_memory(include_bytes!(concat!($svg)))
-        });
+        static $name: LazyLock<Handle> =
+            LazyLock::new(|| Handle::from_memory(include_bytes!(concat!($svg))));
     };
 }
 
@@ -19,37 +25,72 @@ icon!(MAXIMIZE_ICON, "win/maximize.svg");
 icon!(MAXIMIZE_RESTORE_ICON, "win/maximize_restore.svg");
 icon!(CLOSE_ICON, "win/close.svg");
 
-fn transparent_button<'a>(icon: Handle, message: MainMessage) -> Element<'a, MainMessage>
-{
-    medium(Svg::new(icon))
-        .style(|theme, status| {
-            button::Style {
-                background: None,
-                text_color: Color::WHITE,
-                ..Default::default()
+#[derive(Clone)]
+enum ControlType {
+    Minimize,
+    Maximize(bool),
+    Close,
+}
+
+fn transparent_button<'a>(ctype: ControlType, message: MainMessage) -> Element<'a, MainMessage> {
+    let icon = match &ctype {
+        ControlType::Minimize => MINIMIZE_ICON.clone(),
+        ControlType::Maximize(restore) => {
+            if *restore {
+                MAXIMIZE_RESTORE_ICON.clone()
+            } else {
+                MAXIMIZE_ICON.clone()
             }
+        }
+        ControlType::Close => CLOSE_ICON.clone(),
+    };
+
+    let svg_icon = Svg::new(icon).width(10).style(|_, _| svg::Style {
+        color: Some(Color::WHITE),
+    });
+
+    let container = Container::new(svg_icon)
+        .align_x(Horizontal::Center)
+        .align_y(Vertical::Center)
+        .height(Length::Fill)
+        .width(Length::Fill);
+
+    medium(container)
+        .style(move |_, status| button::Style {
+            background: match (ctype.clone(), status) {
+                (ControlType::Close, button::Status::Hovered) => {
+                    Some(iced::Background::Color(Color::from_rgb8(196, 43, 28)))
+                }
+                (_, button::Status::Hovered) => {
+                    Some(iced::Background::Color(Color::WHITE.scale_alpha(0.35)))
+                }
+                (_, _) => None,
+            },
+            ..Default::default()
         })
-        .on_press(message).into()
+        .width(46)
+        .height(32)
+        .padding(0)
+        .on_press(message)
+        .into()
 }
 
 pub fn window_controls<'a>(
     show_minimze: bool,
     show_maximize: bool,
     is_maximized: bool,
-    id: window::Id
-) -> Element<'a, MainMessage>
-{
+    id: window::Id,
+) -> Element<'a, MainMessage> {
     row![
-        show_minimze.then_some(
-            transparent_button(MINIMIZE_ICON.clone(), MainMessage::WindowMinimize(id))
-        ),
+        show_minimze.then_some(transparent_button(
+            ControlType::Minimize,
+            MainMessage::WindowMinimize(id)
+        )),
         show_maximize.then_some(transparent_button(
-            if is_maximized {
-                MAXIMIZE_RESTORE_ICON.clone()
-            } else {
-                MAXIMIZE_ICON.clone()
-            }, MainMessage::WindowMaximize(id))
-        ),
-        transparent_button(CLOSE_ICON.clone(), MainMessage::WindowClose(id))
-    ].into()
+            ControlType::Maximize(is_maximized),
+            MainMessage::WindowMaximize(id)
+        )),
+        transparent_button(ControlType::Close, MainMessage::WindowClose(id))
+    ]
+    .into()
 }
